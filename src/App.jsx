b6274950c67154157a9143929f4467f50a56fee0
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2023-10-03 03:05:06 +0000

    initial push

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Michelle Sandford",
+  title: "Microsoft Developer Engagement Lead Asia ANZ",
+  email: "",
+  gitHub: "codess-aus",
+  instagram: "codess_aus",
+  linkedIn: "michellesandford",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "codess_aus",
+  youTube: "michellesandford",
 };
 
 const primaryColor = "#4E567E";
--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -16,27 +16,27 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a image you
  * freely use on your site.
  */
-import image from "../images/motion-background.jpg";
+import image from "../images/nuevo.png";
 
-const imageAltText = "purple and blue abstract background";
+const imageAltText = "blue robot and some code";
 
 /**
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a UI/UX student studying at Barnett Technical University. I enjoy creating unique and simplistic user interfaces in creative ways.";
+  "I am a developer engagement lead for Microsoft. I love helping enable students to do more bu building on Azure with GitHub Codespaces. I am passionate about solving problems in new creative ways to drive innovation.";
 
 /**
  * List of some of skills or technologies you work on, are learning,
  * passionate about, or enjoy,
  */
 const skillsList = [
-  "Web design",
-  "User experience",
-  "Inclusive design",
-  "Focus group testing",
-  "Mobile user interfaces",
-  "Graphic design",
+  "Public Speaker",
+  "Pythonista",
+  "Azure Community Hero",
+  "She Codes Mentor",
+  "Guest Lecturer",
+  "AI Chatbot builder",
 ];
 
 /**
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  "I am passionate about solving problems in new creative ways to drive innovation.";
 
 const About = () => {
   return (
