8e49c2b1dc5cdf906aca6e7a9c0fc033a7e296a4
Michelle Mei-Ling Sandford <codesswa@outlook.com>
2023-11-29 16:23:34 +1100

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,20 +20,22 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/GenZ_Gaming.png";
+import image from "../images/NDC.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "Adult female speaking at a conference";
+
+import './Home.css';
 
 const Home = ({ name, title }) => {
   return (
-    <section id="home" className="min-height">
-      <img className="background" src={image} alt="" />
-      <div style={{ position: "absolute", top: "5rem", left: "2rem", width: "17rem" }}>
+    <section id="home" className="home"> 
+      <img className="home__background" src={image} alt="" />
+      <div className="home__header">
         <h1>{name}</h1>
         <h2>{title}</h2>
       </div>
-      <div style={{ position: "absolute", bottom: "3rem", left: "50%" }}>
-        <img src={arrowSvg} style={{ height: "3rem", width: "3rem" }} alt={imageAltText} />
+      <div className="home__arrow">
+        <img src={arrowSvg} alt={imageAltText} />
       </div>
     </section>
   );
